f6c128ed7294925102f8cf2ecb9160d4cd098c8a
volodymyr-hamalii <hamalii.volodymyr@gmail.com>
2024-08-11 21:33:57 +0300

    Refactor: move plot building logic to the separate method

--- src/structure_visualizer/structure_visualizer.py
+++ src/structure_visualizer/structure_visualizer.py
@@ -27,20 +27,15 @@ class StructureVisualizer:
         fig: Figure = plt.figure()
         ax: Axes = fig.add_subplot(111, projection='3d')
 
-        x: ndarray = coordinates[:, 0]
-        y: ndarray = coordinates[:, 1]
-        z: ndarray = coordinates[:, 2]
-
-        # Plot the atoms
-        ax.scatter(x, y, z, s=visual_parameters.size, c=visual_parameters.color_atoms)  # type: ignore
-        cls._set_equal_scaling(ax, x, y, z)
-
-        if to_build_bonds:
-            LinesBuilder.add_lines(
-                coordinates=coordinates, ax=ax,
-                color_bonds=visual_parameters.color_bonds,
-                num_of_min_distances=num_of_min_distances,
-                skip_first_distances=skip_first_distances)
+        cls._plot_atoms(
+            ax=ax,
+            coordinates=coordinates,
+            visual_parameters=visual_parameters,
+            to_set_scaling=True,
+            to_build_bonds=to_build_bonds,
+            num_of_min_distances=num_of_min_distances,
+            skip_first_distances=skip_first_distances,
+        )
 
         ax.set_xlabel('X')
         ax.set_ylabel('Y')
@@ -64,43 +59,21 @@ class StructureVisualizer:
         ax: Axes = fig.add_subplot(111, projection='3d')
 
         # Plot first structure atoms (carbone by default)
-        x_first: ndarray = coordinates_first[:, 0]
-        y_first: ndarray = coordinates_first[:, 1]
-        z_first: ndarray = coordinates_first[:, 2]
-        cls._set_equal_scaling(ax, x_first, y_first, z_first)
-
-        ax.scatter(
-            x_first, y_first, z_first,
-            c=visual_parameters_first.color_atoms,
-            label=visual_parameters_first.label,
-            s=visual_parameters_first.size,  # type: ignore
-            alpha=visual_parameters_first.transparency)
+        cls._plot_atoms(
+            ax=ax,
+            coordinates=coordinates_first,
+            visual_parameters=visual_parameters_first,
+            to_set_scaling=True,
+            to_build_bonds=to_build_bonds)
 
         # Plot second structure atoms (al by default)
-        x_second: ndarray = coordinates_second[:, 0]
-        y_second: ndarray = coordinates_second[:, 1]
-        z_second: ndarray = coordinates_second[:, 2]
-
-        ax.scatter(
-            x_second, y_second, z_second,
-            c=visual_parameters_second.color_atoms,
-            label=visual_parameters_second.label,
-            s=visual_parameters_second.size,  # type: ignore
-            alpha=visual_parameters_second.transparency)
-
-        if to_build_bonds:
-            # Carbone
-            LinesBuilder.add_lines(
-                coordinates=coordinates_first, ax=ax,
-                color_bonds=visual_parameters_first.color_bonds,
-                num_of_min_distances=3)
-
-            # Aluminium
-            LinesBuilder.add_lines(
-                coordinates=coordinates_second, ax=ax,
-                color_bonds=visual_parameters_second.color_bonds,
-                num_of_min_distances=1,
-                skip_first_distances=2)
+        cls._plot_atoms(
+            ax=ax,
+            coordinates=coordinates_second,
+            visual_parameters=visual_parameters_second,
+            to_build_bonds=to_build_bonds,
+            num_of_min_distances=1,
+            skip_first_distances=2)
 
         ax.set_xlabel('X')
         ax.set_ylabel('Y')
@@ -109,8 +82,42 @@ class StructureVisualizer:
 
         plt.show()
 
+    @classmethod
+    def _plot_atoms(
+            cls,
+            ax: Axes,
+            coordinates: ndarray,
+            visual_parameters: StructureVisualParameters,
+            to_set_scaling: bool = False,
+            to_build_bonds: bool = True,
+            num_of_min_distances: int = 3,
+            skip_first_distances: int = 0) -> None:
+
+        x_first: ndarray = coordinates[:, 0]
+        y_first: ndarray = coordinates[:, 1]
+        z_first: ndarray = coordinates[:, 2]
+
+        if to_set_scaling:
+            cls._set_equal_scaling(ax, x_first, y_first, z_first)
+
+        ax.scatter(
+            x_first, y_first, z_first,
+            c=visual_parameters.color_atoms,
+            label=visual_parameters.label,
+            s=visual_parameters.size,  # type: ignore
+            alpha=visual_parameters.transparency)
+
+        if to_build_bonds:
+            # Carbone
+            LinesBuilder.add_lines(
+                coordinates=coordinates, ax=ax,
+                color_bonds=visual_parameters.color_bonds,
+                num_of_min_distances=num_of_min_distances,
+                skip_first_distances=skip_first_distances)
+
     @staticmethod
     def _set_equal_scaling(ax: Axes, x_coor: ndarray, y_coor: ndarray, z_coor: ndarray) -> None:
+        """ Set equal scaling for all axis """
 
         max_range = np.array([
             x_coor.max() - x_coor.min(),
