fe26623a3b8f2d0d955afc21c37dffd906db7441
volodymyr-hamalii <hamalii.volodymyr@gmail.com>
2024-08-12 23:29:16 +0300

    Fix: use StructureSettingsManager in Actions

--- src/actions.py
+++ src/actions.py
@@ -2,7 +2,7 @@ from numpy import ndarray
 
 from .utils import PathBuilder, FilesConverter, Logger
 from .structure_visualizer import StructureVisualizer, AtomsUniverseBuilder, VisualizationParameters
-from .coordinates_actions import CoordinateLimits
+from .data_preparation import StructureSettings, StructureSettingsManager, ChannelLimits
 from .intercalation import IntercalatedChannelBuilder
 
 logger = Logger(__name__)
@@ -30,10 +30,10 @@ class Actions:
 
     @staticmethod
     def convert_init_dat_to_pdb(structure_folder: str) -> None:
+        FilesConverter.dat_to_pdb(structure_folder=structure_folder)
 
-        FilesConverter.dat_to_pdb(
-            structure_folder=structure_folder,
-        )
+        # Create template for coordinates if it doesn't exists
+        StructureSettingsManager.create_structure_settings_template(structure_folder=structure_folder)
 
     @staticmethod
     def show_init_structure(structure_folder: str) -> None:
@@ -45,34 +45,32 @@ class Actions:
     def show_init_al_structure() -> None:
         path_to_al_pdb_file: str = PathBuilder.build_path_to_init_data_file(file="al.pdb")
         coordinates: ndarray = AtomsUniverseBuilder.builds_atoms_coordinates(path_to_al_pdb_file)
+
         StructureVisualizer.show_structure(
             coordinates=coordinates,
             to_build_bonds=True,
             visual_parameters=VisualizationParameters.al,
             num_of_min_distances=1,
-            skip_first_distances=2,
-        )
+            skip_first_distances=2)
 
     @staticmethod
     def show_one_channel_structure(structure_folder: str) -> None:
         path_to_init_pdb_file: str = PathBuilder.build_path_to_result_data_file(structure_folder)
 
-        channel_coordinate_limits = CoordinateLimits(
-            x_min=-2.00,
-            x_max=4.50,
-            y_min=-0.40,
-            y_max=4.8,
-        )
+        structure_settings: None | StructureSettings = StructureSettingsManager.read_file(
+            structure_folder=structure_folder)
+
+        channel_limits: ChannelLimits | None = structure_settings.channel_limits if structure_settings else None
 
         coordinates: ndarray = AtomsUniverseBuilder.builds_atoms_coordinates(
-            path_to_init_pdb_file, channel_coordinate_limits)
+            path_to_init_pdb_file, channel_limits)
         StructureVisualizer.show_structure(coordinates, to_build_bonds=True)
 
     @staticmethod
     def show_al_in_one_channel_structure(structure_folder: str) -> None:
         coordinates: tuple[ndarray, ndarray] = IntercalatedChannelBuilder.build_al_in_carbone(
-            structure_folder=structure_folder, filter_al_atoms=True
-        )
+            structure_folder=structure_folder, filter_al_atoms=True)
+
         coordinates_carbone: ndarray = coordinates[0]
         coordinates_al: ndarray = coordinates[1]
 
