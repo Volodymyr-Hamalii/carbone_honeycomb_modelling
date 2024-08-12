6aad64856cf11e3af1c3616e8650a159e2fb3909
volodymyr-hamalii <hamalii.volodymyr@gmail.com>
2024-08-12 23:30:23 +0300

    Fix: run all methods in full_flow

--- src/actions.py
+++ src/actions.py
@@ -84,8 +84,8 @@ class Actions:
 
     @classmethod
     def full_flow(cls, structure_folder: str) -> None:
-        # cls.convert_init_dat_to_pdb(structure_folder)
-        # cls.show_init_structure(structure_folder)
-        # cls.show_init_al_structure()
-        # cls.show_one_channel_structure(structure_folder)
+        cls.convert_init_dat_to_pdb(structure_folder)
+        cls.show_init_structure(structure_folder)
+        cls.show_init_al_structure()
+        cls.show_one_channel_structure(structure_folder)
         cls.show_al_in_one_channel_structure(structure_folder)
