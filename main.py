d74e740d7e4bbaac7470e3ec6578023309220fa2
volodymyr-hamalii <hamalii.volodymyr@gmail.com>
2024-08-17 15:06:26 +0300

    Fix: process additional optional params

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ DEFAULT_STRUCTURE_FOLDER = "A1-7_h3"
 def main() -> None:
     action: str = sys.argv[1] if len(sys.argv) > 1 else DEFAULT_ACTION
     structure_folder: str = sys.argv[2] if len(sys.argv) > 2 else DEFAULT_STRUCTURE_FOLDER
+    args: list[str] = sys.argv[3:] if len(sys.argv) > 3 else []
 
     try:
         logger.info(f"Run '{action}' for '{structure_folder}' structure.")
@@ -18,8 +19,13 @@ def main() -> None:
         # Get the method from the Actions class
         method = getattr(Actions, action)
 
-        # Call the method with the structure_folder parameter
-        method(structure_folder)
+        if args:
+            # Call the method with the structure_folder and other parameters
+            method(structure_folder, *args)
+        else:
+            # Call the method with the structure_folder parameter
+            method(structure_folder)
+
     except AttributeError:
         logger.error(f"Action '{action}' is not available.")
         Actions.help(structure_folder)
