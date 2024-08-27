c541a89329d47c36a4a05c153f636a5a0ff37997
volodymyr-hamalii <hamalii.volodymyr@gmail.com>
2024-08-27 22:53:01 +0300

    Fix: make case agnostic lattice types

--- src/base_structure_classes/lattice_type.py
+++ src/base_structure_classes/lattice_type.py
@@ -1,27 +1,26 @@
 from abc import ABC, abstractmethod
 
+
 class LatticeType(ABC):
     def __init__(self, structure_type: str) -> None:
         """ Base class for different lattice structures """
-        self.is_cell: bool = structure_type == "cell"  # Cubic cell
-        self.is_fcc: bool = structure_type == "FCC"  # Face-centered cubic
-        self.is_hcp: bool = structure_type == "HCP"  # Hexagonal close-packed
+        self.is_cell: bool = structure_type.lower() == "cell"  # Cubic cell
+        self.is_fcc: bool = structure_type.lower() == "fcc"  # Face-centered cubic
+        self.is_hcp: bool = structure_type.lower() == "hcp"  # Hexagonal close-packed
 
         # Check the valid structure types in derived classes
         if not self._is_valid_structure(structure_type):
-            raise ValueError(f"Invalid structure type: {structure_type}. Available options are: {self.get_available_types()}")
-
+            raise ValueError(
+                f"Invalid structure type: {structure_type}. Available options are: {self.get_available_types()}")
 
     def _is_valid_structure(self, structure_type: str) -> bool:
         return structure_type in self.get_available_types()
-
 
     @staticmethod
     @abstractmethod
     def get_info() -> str:
         """ Abstract method to provide information about the lattice type """
         ...
-
 
     @staticmethod
     @abstractmethod
