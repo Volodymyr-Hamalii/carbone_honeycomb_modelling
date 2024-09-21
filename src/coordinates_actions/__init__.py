b131ee2902de68cb322448651767bfbe2f4703b4
volodymyr-hamalii <hamalii.volodymyr@gmail.com>
2024-09-21 23:21:47 +0300

    Chore: file import

--- src/coordinates_actions/__init__.py
+++ src/coordinates_actions/__init__.py
@@ -1,4 +1,5 @@
 from .structure_translator import StructureTranslator
+from .structure_rotator import StructureRotator
 from .planes_builder import PlanesBuilder
 from .coordinates_filter import CoordinatesFilter
 from .point_organizer import PointsOrganizer
