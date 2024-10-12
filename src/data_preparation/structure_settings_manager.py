9b9827a5f6624fbb3a603ce701b82cff0c4b8561
volodymyr-hamalii <hamalii.volodymyr@gmail.com>
2024-10-12 17:04:59 +0300

    Feat: build channel_limits from points_to_set_channel_planes if there are no channel_limits

--- src/data_preparation/structure_settings_manager.py
+++ src/data_preparation/structure_settings_manager.py
@@ -2,7 +2,7 @@ import json
 from pathlib import Path
 
 from ..utils import PathBuilder, FileReader, Constants, Logger
-from .structure_settings_map import StructureSettings, ChannelLimits, ChannelPoints
+from .structure_settings import StructureSettings, ChannelLimits, ChannelPoints
 
 
 logger = Logger("StructureSettingsManager")
@@ -20,25 +20,31 @@ class StructureSettingsManager:
             logger.warning(f"Settings file for {structure_folder} not found")
             return
 
-        # TODO: to automate searching for limits instead of providing them in the file
-        channel_limits: dict = structure_settings["channel_limits"]
+        points_to_set_channel_planes: list[ChannelPoints] = [
+            ChannelPoints(
+                points=points_data["points"],
+                direction=points_data["direction"],
+            ) for points_data in structure_settings.get("points_to_set_channel_planes", [])
+        ]
+
+        # Get channel_limits
+        channel_limits_from_channel_points: ChannelLimits = cls.build_channel_limits_from_channel_points(
+            points_to_set_channel_planes)
+
+        channel_limits_data: dict = structure_settings.get("channel_limits", {})
+
+        channel_limits = ChannelLimits(
+            x_min=channel_limits_data.get("x_min", channel_limits_from_channel_points.x_min),
+            x_max=channel_limits_data.get("x_max", channel_limits_from_channel_points.x_max),
+            y_min=channel_limits_data.get("y_min", channel_limits_from_channel_points.y_min),
+            y_max=channel_limits_data.get("y_max", channel_limits_from_channel_points.y_max),
+            z_min=channel_limits_data.get("z_min", channel_limits_from_channel_points.z_min),
+            z_max=channel_limits_data.get("z_max", channel_limits_from_channel_points.z_max),
+        )
+
         return StructureSettings(
-            channel_limits=ChannelLimits(
-                x_min=channel_limits["x_min"],
-                x_max=channel_limits["x_max"],
-                y_min=channel_limits["y_min"],
-                y_max=channel_limits["y_max"],
-                z_min=channel_limits.get("z_min"),
-                z_max=channel_limits.get("z_max"),
-            ),
-
-            points_to_set_channel_planes=[
-                ChannelPoints(
-                    points=points_data["points"],
-                    direction=points_data["direction"],
-                ) for points_data in structure_settings.get("points_to_set_channel_planes", [])
-            ],
-
+            points_to_set_channel_planes=points_to_set_channel_planes,
+            channel_limits=channel_limits,
             distance_from_plane=structure_settings.get("distance_from_plane", 0),
             max_distance_to_carbon_atoms=structure_settings.get("max_distance_to_carbon_atoms", 0),
             al_lattice_parameter=structure_settings.get("al_lattice_parameter", 0),
@@ -47,14 +53,14 @@ class StructureSettingsManager:
     @classmethod
     def create_structure_settings_template(cls, structure_folder: str) -> None:
         structure_settings_template: dict = {
-            "channel_limits": {
-                "x_min": None,
-                "x_max": None,
-                "y_min": None,
-                "y_max": None,
-                "z_min": None,
-                "z_max": None,
-            },
+            # "channel_limits": {
+            #     "x_min": None,
+            #     "x_max": None,
+            #     "y_min": None,
+            #     "y_max": None,
+            #     "z_min": None,
+            #     "z_max": None,
+            # },
             "points_to_set_channel_planes": [
                 {
                     "points": [
@@ -79,3 +85,20 @@ class StructureSettingsManager:
         # Create a structure_settings.json template
         Path(path_to_file).write_text(json.dumps(structure_settings_template, indent=4), encoding="utf-8")
         logger.info(f"Created structure_settings template in {path_to_file}. Please, fill it out.")
+
+    @staticmethod
+    def build_channel_limits_from_channel_points(channel_points: list[ChannelPoints]) -> ChannelLimits:
+        all_points: list[list[float]] = [
+            point
+            for channel_points_item in channel_points
+            for point in channel_points_item.points
+        ]
+
+        return ChannelLimits(
+            x_min=min(point[0] for point in all_points),
+            x_max=max(point[0] for point in all_points),
+            y_min=min(point[1] for point in all_points),
+            y_max=max(point[1] for point in all_points),
+            z_min=min(point[2] for point in all_points),
+            z_max=max(point[2] for point in all_points),
+        )
