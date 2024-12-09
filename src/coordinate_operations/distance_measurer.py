1a0b311ab6ae7a00966ab1a76cb738e2b58b9be6
volodymyr-hamalii <hamalii.volodymyr@gmail.com>
2024-12-09 23:15:42 +0200

    Feat: calculate_distance_between_2_points logic

--- src/coordinate_operations/distance_measurer.py
+++ src/coordinate_operations/distance_measurer.py
@@ -1,19 +1,36 @@
-
+from math import sqrt
 import numpy as np
 from numpy import ndarray, floating
 from scipy.spatial.distance import cdist
 
+
 class DistanceMeasure:
     @staticmethod
+    def calculate_distance_between_2_points(p1: tuple | np.ndarray, p2: tuple | np.ndarray) -> float:
+        """
+        Compute distance between two points (x1, y1, z1) and (x2, y2, z2),
+        where z1 and z2 are optional.
+        """
+        # Add z coordinate if it doesn't exist
+        if len(p1) == 2:
+            p1 = (p1[0], p1[1], 0)
+
+        if len(p2) == 2:
+            p2 = (p2[0], p2[1], 0)
+
+        # Calculate the distance
+        return sqrt((p1[0] - p2[0])**2 + (p1[1] - p2[1])**2 + (p1[2] - p2[2])**2)
+
+    @staticmethod
     def calculate_distance_from_plane(points: ndarray, A: float, B: float, C: float, D: float) -> float:
-        # Calculate the distance of each point from the plane
+        """ Calculate the distance of each point from the plane. """
         numerator: ndarray = np.abs(A * points[:, 0] + B * points[:, 1] + C * points[:, 2] + D)
         denominator = np.sqrt(A**2 + B**2 + C**2)
         return numerator / denominator
 
     @staticmethod
     def calculate_signed_distance_from_plane(points: ndarray, A: float, B: float, C: float, D: float) -> float:
-        # Calculate the signed distance of each point from the plane
+        """ Calculate the signed distance of each point from the plane. """
         numerator: ndarray = A * points[:, 0] + B * points[:, 1] + C * points[:, 2] + D
         denominator = np.sqrt(A**2 + B**2 + C**2)
         return numerator / denominator
