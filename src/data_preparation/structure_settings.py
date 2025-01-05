fc478129fe405d915972df2bb79c35dd1576677b
volodymyr-hamalii <hamalii.volodymyr@gmail.com>
2025-01-05 22:42:56 +0200

    Fix: removed unnecessary from StructureSettings

--- src/data_preparation/structure_settings.py
+++ src/data_preparation/structure_settings.py
@@ -2,25 +2,6 @@ from dataclasses import dataclass
 
 
 @dataclass
-class ChannelLimits:
-    x_min: float
-    x_max: float
-    y_min: float
-    y_max: float
-    z_min: float
-    z_max: float
-
-
-@dataclass
-class ChannelPoints:
-    points: list[list[float]]
-    direction: int
-
-
-@dataclass
 class StructureSettings:
-    channel_limits: ChannelLimits
-    points_to_set_channel_planes: list[ChannelPoints]
     distance_from_plane: float = 0
     max_distance_to_carbon_atoms: float = 0
-    al_lattice_parameter: float = 0
