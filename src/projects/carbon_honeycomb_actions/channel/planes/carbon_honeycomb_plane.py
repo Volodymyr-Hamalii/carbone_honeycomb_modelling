eef8ec50dd38896a8d93325b5f784b8d077d8197
volodymyr-hamalii <hamalii.volodymyr@gmail.com>
2025-01-05 22:44:24 +0200

    Feat: cache properties in CarbonHoneycombPlane and set copy method

--- src/projects/carbon_honeycomb_actions/channel/planes/carbon_honeycomb_plane.py
+++ src/projects/carbon_honeycomb_actions/channel/planes/carbon_honeycomb_plane.py
@@ -1,24 +1,32 @@
 from dataclasses import dataclass
+from functools import cached_property
 
-from src.base_structure_classes import Points
+from src.base_structure_classes import FlatFigure
 
 from .carbon_honeycomb_plane_actions import CarbonHoneycombPlaneActions
 from .plane_polygons import CarbonHoneycombPentagon, CarbonHoneycombHexagon
 
 
 @dataclass
-class CarbonHoneycombPlane(Points):
+class CarbonHoneycombPlane(FlatFigure):
     # Define direction related plane for honeycomb volume:
     # inside - True
     # outside - False
     direction: bool
 
-    @property
+    @cached_property
     def pentagons(self) -> list[CarbonHoneycombPentagon]:
         """ List of the plane pentagons. """
         return list(CarbonHoneycombPlaneActions.define_plane_pentagons(self.points))
 
-    @property
+    @cached_property
     def hexagons(self) -> list[CarbonHoneycombHexagon]:
         """ List of the plane hexagons. """
         return list(CarbonHoneycombPlaneActions.define_plane_hexagons(self.points))
+
+    def copy(self) -> "CarbonHoneycombPlane":
+        """ Returns a new CarbonHoneycombPlane instance. """
+        return CarbonHoneycombPlane(
+            points=self.points.copy(),
+            direction=self.direction,
+        )
