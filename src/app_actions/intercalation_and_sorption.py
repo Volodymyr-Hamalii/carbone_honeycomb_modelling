58a7c11c81e2d88585691d4c8e444a32f179e9d3
volodymyr-hamalii <hamalii.volodymyr@gmail.com>
2025-03-02 21:49:50 +0200

    Fix: removed unnecessary method

--- src/app_actions/intercalation_and_sorption.py
+++ src/app_actions/intercalation_and_sorption.py
@@ -5,7 +5,7 @@ import pandas as pd
 from src.utils import Constants, PathBuilder, FileReader, FileWriter, Logger, Inputs
 from src.base_structure_classes import AlLatticeType, Points, CoordinateLimits
 from src.structure_visualizer import StructureVisualizer
-from src.data_preparation import StructureSettings, StructureSettingsManager
+from src.data_preparation import StructureSettings
 from src.projects import (
     IntercalatedChannelBuilder,
     # AlAtomsFilter,
@@ -24,69 +24,6 @@ logger = Logger("Actions")
 
 
 class AppActionsIntercalationAndSorption:
-    @classmethod
-    def show_al_in_one_channel_structure(cls, structure_folder: str, to_set: bool) -> None:
-        """
-        Build one channel model from result_data/{structure_folder}/ljout-from-init-dat.pdb atoms
-        based on result_data/{structure_folder}/structure_settings.json channel limits,
-        filled with translated Al structure
-        """
-
-        structure_settings: StructureSettings = StructureSettingsManager.get_structure_settings(
-            structure_folder=structure_folder)
-
-        # Collect data to process
-
-        # Carbon
-        coordinates_carbon: Points = IntercalatedChannelBuilder.build_carbon_coordinates(
-            structure_folder=structure_folder)
-
-        carbon_channels: list[CarbonHoneycombChannel] = CarbonHoneycombActions.split_init_structure_into_separate_channels(
-            coordinates_carbon=coordinates_carbon)
-        carbon_channel: CarbonHoneycombChannel = carbon_channels[0]
-
-        # Aluminium
-        to_open_calculated_atoms: bool = Inputs.bool_input(
-            to_set, default_value=True, text="To open previously calculated atoms (if they exist)",
-            env_id="open_calculated_atoms")
-
-        if to_open_calculated_atoms:
-            # Try to load previously calculated points from the file
-            folder_path: Path = PathBuilder.build_path_to_result_data_dir()
-            try:
-                al_points_data: np.ndarray = FileReader.read_dat_file(
-                    structure_folder=structure_folder, folder_path=folder_path)
-
-                al_points = Points(points=al_points_data)
-
-            except FileNotFoundError:
-                logger.warning(f"Calculated Al points for {structure_folder} not found.")
-                al_points: Points = cls._calculate_al_points(
-                    to_set, structure_settings, carbon_channel)
-
-        else:
-            al_points: Points = cls._calculate_al_points(
-                to_set, structure_settings, carbon_channel)
-
-        logger.info("Number of carbon atoms:", len(carbon_channel))
-        logger.info("Number of al atoms:", len(al_points))
-
-        FileWriter.write_dat_file(al_points.points, structure_folder=structure_folder)
-        to_build_bonds: bool = Inputs.bool_input(
-            to_set,
-            default_value=True,
-            text="To build bonds between atoms",
-            env_id="to_build_bonds",
-        )
-
-        StructureVisualizer.show_two_structures(
-            coordinates_first=carbon_channel.planes[0].points,
-            coordinates_second=al_points.points,
-            to_build_bonds=to_build_bonds,
-            title=structure_folder,
-            show_coordinates=True,
-        )
-
     @staticmethod
     def fill_all_channels(structure_folder: str, to_set: bool) -> None:
         """
@@ -456,4 +393,3 @@ class AppActionsIntercalationAndSorption:
         AppActionsShowInitData.show_init_structure(structure_folder, to_set)
         AppActionsShowInitData.show_one_channel_structure(structure_folder, to_set)
         # cls.show_filtered_al_one_channel_structure(structure_folder, to_set)
-        cls.show_al_in_one_channel_structure(structure_folder, to_set)
