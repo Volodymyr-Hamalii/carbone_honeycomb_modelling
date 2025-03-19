1aa15da9a7bbfe3c62f9d1f39cec6737d1b02d4a
volodymyr-hamalii <hamalii.volodymyr@gmail.com>
2025-03-19 13:57:29 +0200

    Fix: correct distance_from_center_to_plane getting in get_channel_details

--- src/app_actions/show_init_data.py
+++ src/app_actions/show_init_data.py
@@ -211,7 +211,7 @@ class AppActionsShowInitData:
             line_equation: tuple[float, float, float, float] = LinesOperations.get_line_equation(min_point, max_point)
 
             distance_from_center_to_plane: float = DistanceMeasure.calculate_distance_from_plane(
-                np.array([center_2d]), line_equation)[0]  # type: ignore
+                np.array([center_2d]), line_equation)
             # distances_from_center_to_planes.append(distance_from_center_to_plane)
 
             # Center of the plane
