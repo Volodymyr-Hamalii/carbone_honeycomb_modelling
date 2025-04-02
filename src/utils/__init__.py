d777d7719e3cee04985889273bce2186aae6b1b4
volodymyr-hamalii <hamalii.volodymyr@gmail.com>
2025-04-02 23:51:55 +0300

    Chore: import SQLiteManager

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -3,5 +3,5 @@ from .files_manager.path_builder import PathBuilder
 from .logger import Logger, execution_time_logger
 from .inputs import Inputs
 from .command_line_args_handler import CommandLineArgsHandler
-
+from .sqlite_manager import SQLiteManager
 from .files_manager import *
