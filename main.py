961a545cb0a9efe7f87e5079dac4bfdc1f01f601
volodymyr-hamalii <hamalii.volodymyr@gmail.com>
2025-04-04 19:46:04 +0300

    Fix: comment CLI logic in main.py

--- main.py
+++ main.py
@@ -1,51 +1,55 @@
-import sys
-from typing import Callable
+# import sys
+# from typing import Callable
 from src import (
-    Constants,
+    # Constants,
     Logger,
-    CommandLineArgsHandler,
-    cli,
+    # CommandLineArgsHandler,
+    # AppActions,
+    AppGui,
 )
 
 
 logger = Logger("Main")
 
 
-def run_action() -> None:
-    action: str = CommandLineArgsHandler.get_str_arg(1, default=Constants.DEFAULT_ACTION)
-    structure_folder: str = CommandLineArgsHandler.get_str_arg(2, default=Constants.DEFAULT_STRUCTURE_FOLDER)
-    to_set: bool = CommandLineArgsHandler.get_bool_arg(3, to_compare="set", default=False)
-    args: list[str] = sys.argv[4:]
+# def run_action() -> None:
+#     action: str = CommandLineArgsHandler.get_str_arg(1, default=Constants.DEFAULT_ACTION)
+#     structure_folder: str = CommandLineArgsHandler.get_str_arg(2, default=Constants.DEFAULT_STRUCTURE_FOLDER)
+#     to_set: bool = CommandLineArgsHandler.get_bool_arg(3, to_compare="set", default=False)
+#     args: list[str] = sys.argv[4:]
 
-    if action == "help":
-        cli.AppActions.help(structure_folder)
-        return
+#     if action == "help":
+#         AppActions.help(structure_folder)
+#         return
 
-    try:
-        logger.info(f"Run '{action}' for '{structure_folder}' structure.")
+#     try:
+#         logger.info(f"Run '{action}' for '{structure_folder}' structure.")
 
-        # Get the method from the AppActions class
-        method: Callable = getattr(cli.AppActions, action)
+#         # Get the method from the AppActions class
+#         method: Callable = getattr(AppActions, action)
 
-        if args:
-            # Call the method with the structure_folder and other parameters
-            method(structure_folder, to_set, *args)
-        else:
-            # Call the method with the structure_folder sparameter
-            method(structure_folder, to_set)
+#         if args:
+#             # Call the method with the structure_folder and other parameters
+#             method(structure_folder, to_set, *args)
+#         else:
+#             # Call the method with the structure_folder sparameter
+#             method(structure_folder, to_set)
 
-    except KeyboardInterrupt:
-        print()
-        logger.info("Keyboard interrupt.")
+#     except KeyboardInterrupt:
+#         print()
+#         logger.info("Keyboard interrupt.")
 
-    except AttributeError:
-        logger.error(f"Action '{action}' is not available.")
-        cli.AppActions.help(structure_folder)
+#     except AttributeError:
+#         logger.error(f"Action '{action}' is not available.")
+#         AppActions.help(structure_folder)
 
-    except TypeError as e:
-        logger.error(f"Parameters error:", e)
-        cli.AppActions.help(structure_folder)
+#     except TypeError as e:
+#         logger.error(f"Parameters error:", e)
+#         AppActions.help(structure_folder)
 
 
 if __name__ == "__main__":
-    run_action()
+    app = AppGui()
+    app.mainloop()
+
+    # run_action()
