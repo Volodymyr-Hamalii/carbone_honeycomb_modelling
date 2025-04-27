5f00461246e4c582ef2c417c7a1f982c35901039
volodymyr-hamalii <hamalii.volodymyr@gmail.com>
2025-04-28 00:09:02 +0300

    Chore: comments and annotations in FlatFigure

--- src/base_structure_classes/flat_figure.py
+++ src/base_structure_classes/flat_figure.py
@@ -11,16 +11,16 @@ from .points import Points
 class FlatFigure(Points):
     @cached_property
     def plane_params(self) -> tuple[float, float, float, float]:
-        """ Compute and cache result of get_plane_params() method. """
+        """ Compute and cache the result of the get_plane_params() method. """
         return self.get_plane_params()
 
     def get_plane_params(self) -> tuple[float, float, float, float]:
         """
-        Define the plane like Ax + By + Cz + D = 0
+        Define the plane like ax + by + cz + d = 0
         using the three provided points.
 
-        Takes 3 points as a parameters as lists with 3 coordinates.
-        Returns A, B, C, D parameters from the equation above.
+        Takes 3 non collinear points from self.points to define the plane.
+        Returns a, b, c, d parameters from the equation above.
         """
 
         points: np.ndarray = self.points
@@ -29,18 +29,19 @@ class FlatFigure(Points):
             raise ValueError("At least 3 points are required to define a plane.")
 
         # 1) Pick first point p1
-        p1 = points[0]
+        p1: np.ndarray = points[0]
 
         # 2) Find a second point p2 that is not identical to p1
-        p2 = None
+        p2: np.ndarray | None = None
         for i in range(1, len(points)):
             if not np.allclose(points[i], p1):
                 p2 = points[i]
                 break
+
         if p2 is None:
             raise ValueError("All points are identical. Cannot define a plane.")
 
-        p3 = None
+        p3: np.ndarray | None = None
         for j in reversed(range(1, len(points))):
             c = points[j]
             # We already used p1, p2, but we might revisit them in the loop;
